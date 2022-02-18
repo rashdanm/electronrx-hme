d6641e0a40585432a32d6fdf0c94af3a45a3e840
RashBM <57621825+RashBM@users.noreply.github.com>
2022-02-18 09:48:11 +0000

    clean up + layout plan

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Hero /> */}
+      {/* <Section /> */}
+      {/* <LearnMore /> */}
+      {/* <Awards /> */}
+      {/* <Contact /> */}
+    </div>
+  );
+};
+
+export default Home;
